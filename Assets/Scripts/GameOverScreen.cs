c6cbfea3a42350dedba882b4a8e07e9b624b69f8
Fouad Animashaun <ayofouad@gmail.com>
2021-09-16 20:16:25 +0100

    Added Death Screen
    Added Death Screen

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public GameObject GameOverUI;
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "DeathCollider")
+        {
+            Debug.Log("OnCollisionEnter2Ddddd");
+            GameOverUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void RestartButton() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenuButton() {
+        SceneManager.LoadScene(0);
+    }
+
+}
